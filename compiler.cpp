8b7877cc5db71adf5a82c0a144931c8f9f9d7c95
Rob Hess <hessro@oregonstate.edu>
2022-05-31 15:28:56 -0700

    Add code from 5/31 lecture.

--- compiler.cpp
+++ compiler.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/IRBuilder.h"
@@ -9,6 +10,8 @@
 static llvm::LLVMContext TheContext;
 static llvm::IRBuilder<> TheBuilder(TheContext);
 static llvm::Module* TheModule;
+
+static std::map<std::string, llvm::Value*> TheSymbolTable;
 
 llvm::Value* numericConstant(float val) {
   return llvm::ConstantFP::get(TheContext, llvm::APFloat(val));
@@ -28,10 +31,63 @@ llvm::Value* binaryOperation(llvm::Value* lhs, llvm::Value* rhs, char op) {
       return TheBuilder.CreateFMul(lhs, rhs, "multmp");
     case '/':
       return TheBuilder.CreateFDiv(lhs, rhs, "divtmp");
+    case '<':
+      lhs = TheBuilder.CreateFCmpULT(lhs, rhs, "lttmp");
+      return TheBuilder.CreateUIToFP(
+        lhs,
+        llvm::Type::getFloatTy(TheContext),
+        "booltmp"
+      );
     default:
       std::cerr << "Invalid operator: " << op << std::endl;
       return NULL;
   }
+}
+
+llvm::Value* generateEntryBlockAlloca(std::string name) {
+  llvm::Function* currFn = TheBuilder.GetInsertBlock()->getParent();
+  llvm::IRBuilder<> tmpBuilder(
+    &currFn->getEntryBlock(),
+    currFn->getEntryBlock().begin()
+  );
+  return tmpBuilder.CreateAlloca(
+    llvm::Type::getFloatTy(TheContext), 0, name.c_str()
+  );
+}
+
+llvm::Value* assignmentStatement(std::string lhs, llvm::Value* rhs) {
+  if (!rhs) {
+    return NULL;
+  }
+
+  if (!TheSymbolTable.count(lhs)) {
+    TheSymbolTable[lhs] = generateEntryBlockAlloca(lhs);
+  }
+  return TheBuilder.CreateStore(rhs, TheSymbolTable[lhs]);
+}
+
+llvm::Value* variableValue(std::string name) {
+  llvm::Value* ptr = TheSymbolTable[name];
+  if (!ptr) {
+    std::cerr << "Unknown variable name: " << name << std::endl;
+    return NULL;
+  }
+  return TheBuilder.CreateLoad(
+    llvm::Type::getFloatTy(TheContext),
+    ptr,
+    name.c_str()
+  );
+}
+
+llvm::Value* ifElseStatement() {
+  llvm::Value* cond = binaryOperation(
+    variableValue("b"),
+    numericConstant(8),
+    '<'
+  );
+  cond = TheBuilder.CreateFCmpONE(
+    cond, numericConstant(0), "ifcond"
+  );
 }
 
 int main(int argc, char const *argv[]) {
@@ -61,8 +117,17 @@ int main(int argc, char const *argv[]) {
     '+'
   );
 
+  llvm::Value* assign1 = assignmentStatement("a", expr2);
+
+  llvm::Value* expr3 = binaryOperation(
+    variableValue("a"),
+    numericConstant(4),
+    '/'
+  );
+  llvm::Value* assign2 = assignmentStatement("b", expr3);
+
   // TheBuilder.CreateRetVoid();
-  TheBuilder.CreateRet(expr2);
+  TheBuilder.CreateRet(variableValue("b"));
 
   llvm::verifyFunction(*foo);
   TheModule->print(llvm::outs(), NULL);
